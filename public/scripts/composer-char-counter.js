fef2823722212d481412a86e24c7f0ad08c4d8e2
josh <nomore2pick@gmail.com>
2019-01-23 10:35:11 -0800

    tweet box set up

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,7 +4,7 @@ $(function () {
     let counter = 140 - $(this).val().length;
     $(this).siblings(".counter").text(counter);
     let counterNum = $(this).siblings(".counter")
-    console.log(counter);
+    console.log($(this).siblings(".counter"));
     if (counter < 0) {
       $(this).siblings(".counter").css("color", "red");
     } else {$(this).siblings(".counter").css("color", "black"); }
