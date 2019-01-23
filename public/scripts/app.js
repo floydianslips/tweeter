6069dc45a4db3a431d33de7323ea2cd706b1cf1e
josh <nomore2pick@gmail.com>
2019-01-23 15:50:01 -0800

    new tweets appear

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,81 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(function () {
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "fkjllllllllllllessssssssssssssssssssssssssseffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+    }, "created_at": 1461116232227 
+    },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
+
+function renderTweets(tweets) {
+//   console.log($("#tweet-container"))
+  tweets.forEach(function(entry) {
+    let cookedBird = createTweetElement(entry)
+    $("#tweet-container").append(cookedBird);
+//     console.log(cookedBird)    
+   })
+//   console.log(renderTweets(data))
+    }
+
+// console.log(renderTweets(tweets))
+
+function createTweetElement (data) {
+  let $tweetSection = $("<section class='tweet'>");
+  let $tweetHeader = $("<header class='tweet'>");
+  let $tweetUser = $("<h2 class='tweet'>").text(data.user.name)
+  let $tweetHandle = $("<p class='tweet'>").text(data.user.handle);
+  let $tweetImg = $("<img src=" + data.user.avatars.small + " class='tweet'>");
+  let $tweetContent = $("<h3 class='tweet'>").text(data.content.text);
+  let $tweetFooter = $("<footer class='tweet'>");
+  let $tweetDate = $("<h4 class='tweet'>").text(data.created_at);
+  
+  let $footer = $tweetFooter.append($tweetDate);
+  let $header = $tweetHeader.append($tweetImg).append($tweetUser).append($tweetHandle);
+  let $fullTweet = $tweetSection.append($header).append($tweetContent).append($footer);
+//   let newTweets = $("#tweet-container").append($fullTweet);
+//  console.log($("fullTweets"))
+  return $fullTweet;
+}
+ renderTweets(data)
+});
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,10 +3,9 @@ $(function () {
   $("textarea").on("input", function() {
     let counter = 140 - $(this).val().length;
     $(this).siblings(".counter").text(counter);
-    let counterNum = $(this).siblings(".counter")
-    console.log($(this).siblings(".counter"));
+    let counterNum = $(this).siblings(".counter");
     if (counter < 0) {
-      $(this).siblings(".counter").css("color", "red");
+     $(this).siblings(".counter").css("color", "red");
     } else {$(this).siblings(".counter").css("color", "black"); }
-  });
+   });
 });
